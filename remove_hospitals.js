2c7a193a770bfdf0e8b94f6d6d8a2b5cffb52340
mjessie <578375250@qq.com>
2026-04-20 21:46:02 +0800

    清除中间文件

--- remove_hospitals.js
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-let data = fs.readFileSync('d:/RD2026/bigscreen/js/data/mock-data.js', 'utf8');
-let t = data.indexOf("id: 'wuhan'");
-if(t > -1) {
-   let idx = data.lastIndexOf("{", t);
-   let before = data.substring(0, idx).trim();
-   if(before.endsWith(",")) { before = before.substring(0, before.length-1); }
-   fs.writeFileSync('d:/RD2026/bigscreen/js/data/mock-data.js', before + '\n    ]\n};\n', 'utf8');
-}
